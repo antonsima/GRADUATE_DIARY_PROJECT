bdfa66cceffd10028ce326ce09105fe03a20037a
Anton <anton_sima@mail.ru>
2025-09-13 00:55:57 +0500

    Реализация отправки уведомлений пользователю в телеграм о записях с определенными тегами

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 
+from diary.models import Entry, Tag
 from users.models import User
 
 admin.site.register(User)
+admin.site.register(Entry)
+admin.site.register(Tag)
