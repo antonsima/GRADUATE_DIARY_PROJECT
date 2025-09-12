c21cd7fa72d45ffda4d87832359ae5670e284d5d
Anton <anton_sima@mail.ru>
2025-09-13 01:11:23 +0500

    Реализация отправки уведомлений пользователю в телеграм о записях с определенными тегами

--- diary/context_processors.py
+++ diary/context_processors.py
@@ -6,38 +6,6 @@ from django.utils import timezone
 from diary.models import Entry, Tag
 
 
-def popular_tags(request):
-    context = {}
-
-    if request.user.is_authenticated:
-        user = request.user
-
-        popular_tags = (
-            Tag.objects.filter(Q(owner=user) | Q(owner__isnull=True), entry__owner=user)
-            .annotate(count=Count("entry"))
-            .order_by("-count")[:10]
-        )
-
-        context["sidebar_popular_tags"] = popular_tags
-
-        entries = Entry.objects.filter(owner=user)
-        total_entries = entries.count()
-
-        now = timezone.now()
-        monthly_entries = entries.filter(
-            entry_date__year=now.year, entry_date__month=now.month
-        ).count()
-
-        today = date.today()
-        today_entries = entries.filter(entry_date=today).count()
-
-        context["sidebar_total_entries"] = total_entries
-        context["sidebar_monthly_entries"] = monthly_entries
-        context["sidebar_today_entries"] = today_entries
-
-    return context
-
-
 def diary_context(request):
     context = {}
 
